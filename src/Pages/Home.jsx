fb9871931c1de7914448b5d2fb3015e03f702d8e
MarkeneverDai <markenever.d@gmail.com>
2024-03-18 18:57:48 -0700

    Co-authored-by: Robert Balatbat <robjgb@users.noreply.github.com>

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, {useState}  from 'react'
+
+{/* Fourth Component of Home Page (Faq Questions) */}
+const Item = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="border rounded shadow-sm">
+      <button
+        type="button"
+        aria-label="Open item"
+        title="Open item"
+        className="flex items-center justify-between w-full p-4 focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p className="text-lg font-medium">{title}</p>
+        <div className="flex items-center justify-center w-8 h-8 border rounded-full">
+          <svg
+            viewBox="0 0 24 24"
+            className={`w-3 text-gray-600 transition-transform duration-200 ${
+              isOpen ? 'transform rotate-180' : ''
+            }`}
+          >
+            <polyline
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+              points="2,7 12,17 22,7"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
+      </button>
+      {isOpen && (
+        <div className="p-4 pt-0">
+          <p className="text-gray-700">{children}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default function Home() {
+  
   return (
+  <> 
+  {/* Top Component of Home Page (Intro), fragments allow you to group elements without a group list */}
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-5 row-gap-8 lg:grid-cols-2">
         <div className="flex flex-col justify-center">
@@ -28,7 +72,7 @@ export default function Home() {
           <a
             href="/"
             aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-brand-secondary hover:text-deep-purple-800"
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-brand-secondary hover:text-brand-tertiary"
           >
             Learn more
             <svg
@@ -68,6 +112,345 @@ export default function Home() {
         </div>
       </div>
     </div>
+  {/* Second Component of Home Page (Facts and uses)*/}
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="bebc38d1-bf72-4c77-a073-f0fe5abe0753"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#bebc38d1-bf72-4c77-a073-f0fe5abe0753)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">The</span>
+          </span>{' '}
+          quick, brown fox jumps over a lazy dog
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
+      </div>
+      <div className="grid gap-8 row-gap-12 lg:grid-cols-2">
+        <div className="max-w-md sm:mx-auto sm:text-center">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-brand-primary sm:mx-auto sm:w-24 sm:h-24">
+            <svg
+              className="w-12 h-12 text-brand-tertiary sm:w-20 sm:h-20"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 className="mb-3 text-xl font-bold leading-5">A business big</h6>
+          <p className="mb-3 text-sm text-gray-900">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium rem aperiam, eaque ipsa quae. Sed ut perspiciatis unde
+            omnis iste natus error sit voluptatem accusantium doloremque rem
+            aperiam, eaque ipsa quae.
+          </p>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-brand-secondary hover:text-brand-tertiary"
+          >
+            Learn more
+          </a>
+        </div>
+        <div className="max-w-md sm:mx-auto sm:text-center">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-brand-primary sm:mx-auto sm:w-24 sm:h-24">
+            <svg
+              className="w-12 h-12 text-brand-tertiary sm:w-20 sm:h-20"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          <h6 className="mb-3 text-xl font-bold leading-5">The quick dog</h6>
+          <p className="mb-3 text-sm text-gray-900">
+            Yolo ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
+            suscipit leo. Carpe diem vulputate est nec commodo rutrum.
+            Pellentesque mattis convallis nisi eu and I ain’t stoppin until the
+            swear jar’s full.
+          </p>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-brand-secondary hover:text-brand-tertiary"
+          >
+            Learn more
+          </a>
+        </div>
+      </div>
+    </div>
+  {/* Third Component of Home Page (Step by Step Process) */}
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-brand-tertiary uppercase rounded-full bg-brand-primary">
+            Brand new
+          </p>
+        </div>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="d0d83814-78b6-480f-9a5f-7f637616b267"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#d0d83814-78b6-480f-9a5f-7f637616b267)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">The</span>
+          </span>{' '}
+          quick, brown fox jumps over a lazy dog
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
+      </div>
+      <div className="relative grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-4 sm:grid-cols-2">
+        <div className="absolute inset-0 flex items-center justify-center sm:hidden lg:flex">
+          <div className="w-px h-full bg-gray-300 lg:w-full lg:h-px" />
+        </div>
+        <div className="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div className="flex items-center justify-between mb-2">
+            <p className="text-lg font-bold leading-5">Believe</p>
+            <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-brand-tertiary bg-brand-primary">
+              1
+            </p>
+          </div>
+          <p className="text-sm text-gray-900">
+            Skate ipsum dolor sit amet, alley oop vert mute-air Colby Carter
+            flail 180 berm.
+          </p>
+        </div>
+        <div className="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div className="flex items-center justify-between mb-2">
+            <p className="text-lg font-bold leading-5">Be yourself</p>
+            <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-brand-tertiary bg-brand-primary">
+              2
+            </p>
+          </div>
+          <p className="text-sm text-gray-900">
+            A flower in my garden, a mystery in my panties. Heart attack never
+            stopped.
+          </p>
+        </div>
+        <div className="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div className="flex items-center justify-between mb-2">
+            <p className="text-lg font-bold leading-5">Just do it</p>
+            <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-brand-tertiary bg-brand-primary">
+              3
+            </p>
+          </div>
+          <p className="text-sm text-gray-900">
+            Chase ball of string eat plants, meow, and throw up because I ate
+            plants going.
+          </p>
+        </div>
+        <div className="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
+          <div className="flex items-center justify-between mb-2">
+            <p className="text-lg font-bold leading-5">Eat that</p>
+            <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-brand-tertiary bg-brand-primary">
+              4
+            </p>
+          </div>
+          <p className="text-sm text-gray-900">
+            Bro ipsum dolor sit amet gaper backside single track, manny Bike
+            epic clipless.
+          </p>
+        </div>
+      </div>
+      <div className="text-center">
+        <a
+          href="/"
+          className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-brand-secondary hover:bg-brand-tertiary focus:shadow-outline focus:outline-none"
+        >
+          Learn more
+        </a>
+      </div>
+    </div>
+  {/* Fourth Component of Home Page (Faq Question) */}
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
+        <div class="flex flex-col mb-16 sm:text-center">
+          <a href="/" class="mb-6 sm:mx-auto">
+            <div class="flex items-center justify-center w-12 h-12 rounded-full bg-brand-primary">
+              <svg
+                class="w-10 h-10 text-brand-tertiary"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+          </a>
+          <div class="max-w-xl md:mx-auto sm:text-center lg:max-w-2xl">
+            <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+              <span class="relative inline-block">
+                <svg
+                  viewBox="0 0 52 24"
+                  fill="currentColor"
+                  class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                >
+                  <defs>
+                    <pattern
+                      id="ec5d8ef5-b853-4714-b94f-df28ec98eeb7"
+                      x="0"
+                      y="0"
+                      width=".135"
+                      height=".30"
+                    >
+                      <circle cx="1" cy="1" r=".7" />
+                    </pattern>
+                  </defs>
+                  <rect
+                    fill="url(#ec5d8ef5-b853-4714-b94f-df28ec98eeb7)"
+                    width="52"
+                    height="24"
+                  />
+                </svg>
+                <span class="relative">The</span>
+              </span>{' '}
+              quick, brown fox jumps over a lazy dog
+            </h2>
+            <p class="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque rem aperiam, eaque ipsa quae.
+            </p>
+          </div>
+        </div>
+        <div class="space-y-4">
+          <Item title="The quick, brown fox jumps over a lazy dog?">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque rem aperiam, eaque ipsa quae.
+          </Item>
+          <Item title="The first mate and his Skipper too will do?">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque rem aperiam, eaque ipsa quae.
+          </Item>
+          <Item title="Is the Space Pope reptilian!?">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque rem aperiam, eaque ipsa quae.
+          </Item>
+          <Item title="How much money you got on you?">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque rem aperiam, eaque ipsa quae.
+          </Item>
+        </div>
+      </div>
+    </div>
+
+  {/* Fifth Component of Home Page (Get Started) */}
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
+        <div className="relative lg:w-1/2">
+          <img
+            src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+            className="object-cover w-full lg:absolute h-80 lg:h-full"
+          />
+          <svg
+            className="absolute top-0 right-0 hidden h-full text-white lg:inline-block"
+            viewBox="0 0 20 104"
+            fill="currentColor"
+          >
+            <polygon points="17.3036738 5.68434189e-14 20 5.68434189e-14 20 104 0.824555778 104" />
+          </svg>
+        </div>
+        <div className="flex flex-col justify-center p-8 bg-white lg:p-16 lg:pl-10 lg:w-1/2">
+          <div>
+            <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              Brand new
+            </p>
+          </div>
+          <h5 className="mb-3 text-3xl font-extrabold leading-none sm:text-4xl">
+            Your new ideal style
+          </h5>
+          <p className="mb-5 text-gray-800">
+            <span className="font-bold">Lorem ipsum</span> dolor sit amet,
+            consectetur adipiscing elit. Etiam sem neque, molestie sit amet
+            venenatis et, dignissim ut erat. Sed aliquet velit id dui eleifend,
+            sed consequat odio sollicitudin.
+          </p>
+          <div className="flex items-center">
+            <button
+              type="submit"
+              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+            >
+              Get started
+            </button>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn More
+              <svg
+                className="inline-block w-3 ml-2"
+                fill="currentColor"
+                viewBox="0 0 12 12"
+              >
+                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
