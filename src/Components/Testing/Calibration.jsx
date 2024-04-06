140cff052185e92cab608d798bfe603b046abd07
MikeHi1 <mikeneverd3@gmail.com>
2024-04-05 18:37:55 -0700

    calibration
    Co-authored-by: Robert Balatbat <robjgb@users.noreply.github.com>

--- src/Components/Testing/Calibration.jsx
+++ src/Components/Testing/Calibration.jsx
@@ -6,9 +6,56 @@ export default function Calibration({ setTestState }) {
   }
 
   return (
-    <>
+    <div className="container mx-auto px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 min-h-screen flex flex-col place-content-center">
       <div>Calibration</div>
+
+      <nav className="w-full h-16 bg-white">
+        <div className="grid h-full max-w-lg grid-cols-4 mx-auto border border-gray-200 rounded-full dark:bg-gray-700 dark:border-gray-600">
+          <p
+            data-tooltip-target="tooltip-home"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            Not yet calibrated
+          </p>
+          <button
+            data-tooltip-target="tooltip-wallet"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            Recalibrate
+          </button>
+
+          <button
+            data-tooltip-target="tooltip-settings"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            Toggle Filter
+          </button>
+
+          <button
+            data-tooltip-target="tooltip-profile"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          >
+            Help
+          </button>
+        </div>
+      </nav>
+
+      <div className="calibrationDiv">
+        <input type="button" className="Calibration" id="Pt1" />
+        <input type="button" className="Calibration" id="Pt2" />
+        <input type="button" className="Calibration" id="Pt3" />
+        <input type="button" className="Calibration" id="Pt4" />
+        <input type="button" className="Calibration" id="Pt5" />
+        <input type="button" className="Calibration" id="Pt6" />
+        <input type="button" className="Calibration" id="Pt7" />
+        <input type="button" className="Calibration" id="Pt8" />
+        <input type="button" className="Calibration" id="Pt9" />
+      </div>
+
       <button onClick={handleButtonClick}>Change State in Child</button>
-    </>
+    </div>
   );
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 export default function About() {
+  const [toDos, setToDos] = useState([]);
+  const inputRef = useRef(null)
+
+  function addToDo(e) {
+    e.preventDefault()
+    const inputValue = inputRef.current.value
+    setToDos([...toDos,inputValue])
+  }
+
+useEffect(()=>{
+  console.log(toDos)
+}, [toDos])
+
+  
   return (
-    <div>About</div>
+    <div class="container mx-auto">
+      <form onSubmit={addToDo}>
+        <input ref={inputRef} type="text" className='border'/>
+        <button type="submit">Submit</button>
+      </form>
+
+      {
+        toDos.map(function (toDo) {
+          return (
+            <div> {toDo} </div>
+        )
+      })
+    }
+    </div>
   )
 }
