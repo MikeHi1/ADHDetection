ad92191d0105bd9822dcd5009e02cf539b5d6162
Mark Dai <markenever.d@gmail.com>
2024-04-22 18:06:44 -0700

    Co-authored-by: Robert Balatbat <robjgb@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,21 @@ import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation} from "react-router-dom";
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import { Helmet } from "react-helmet";
 
+
 function App() {
+  let location = useLocation();
+
   const [count, setCount] = useState(0)
   
-  // useEffect(() => {
-  //  const webgazer = window.webgazer
-  //   webgazer.setGazeListener(function(data, elapsedTime) {
-  //     if (data == null) {
-  //       return;
-  //     }
-  //     var xprediction = data.x; //these x coordinates are relative to the viewport
-  //     var yprediction = data.y; //these y coordinates are relative to the viewport
-  //     console.log(elapsedTime); //elapsed time is based on time since begin was called
-  //     console.log(data);
-  //   }).begin();
-  //   console.log()
-  // }, [])
+  useEffect(() => {
+    console.log(location.pathname)
+    
+  }, [location])
 
   return (
     <div className=''> 
@@ -39,8 +33,15 @@ function App() {
       </Helmet>
 
     {/* margins xaxis adjustment */}
-    <Navbar/>
-    <div className = 'container mx-auto'> <Outlet/></div>
+
+    { location.pathname !== "/testing" && 
+      <Navbar/>
+    }
+    
+    
+    <div className = 'container mx-auto pt-16 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8"'> 
+      <Outlet/>
+    </div>
     {/* dsplays different pages */}
     <Footer/>
     </div>
--- src/Components/Testing/Test1.jsx
+++ src/Components/Testing/Test1.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 export default function Test1() {
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('https://api.unsplash.com/photos/random?count=10&client_id=wxFE-eV4eaT1N27OsKq5N8Kt9riF36G9Dy_KSPZenqQ');
+        const data = await response.json();
+        console.log(data);
+        // setImageList(data);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+    // COMMIT TO GITHUB
+  },[]);
+
   return (
     <div>Test1</div>
   )
